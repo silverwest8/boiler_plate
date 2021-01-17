18a812e3f0aaa4f7ac1c2d9db0eb891f403a6e19
silverwest8 <silverwest8@gmail.com>
2021-01-17 18:14:41 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://silverwest:909699@boilerplate.2sfuf.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
