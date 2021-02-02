8a1d8b0dd94a21bd19e142b0df361ad22f86f4c5
silverwest8 <silverwest8@gmail.com>
2021-02-02 23:03:20 +0900

    redux start...

--- client/src/index.js
+++ client/src/index.js
@@ -10,11 +10,14 @@ import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
+import Reducer from "./_reducers";
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 ReactDOM.render(
 
-  <Provider store={createStoreWithMiddleware()}>
+  <Provider store={createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App />
   </Provider>
 
