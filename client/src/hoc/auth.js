b2678d0967fe7c02800ce43545fcdc6a56d7ac52
silverwest8 <silverwest8@gmail.com>
2021-02-04 22:59:34 +0900

    Complete!

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -2,8 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { auth } from '../_actions/user_action';
 
-export default function (SpecificComponent, option, adminRout = null) {
-
+export default function (SpecificComponent, option, adminRoute = null) {
+/*
+    export default function (Specification, option, adminRout = null) 
+    에서 option => null(아무나 출입 가능), true(로그인유저만 출입 가능), false(로그인한 유저는 출입 불가)
+    사용 : Auth( Page, null, true) 마지막true -> 어드민유저만 출입 가능
+*/
     function AthenticationCheck(props) {
 
         const dispatch = useDispatch();
@@ -13,6 +17,21 @@ export default function (SpecificComponent, option, adminRout = null) {
             dispatch(auth()) //action이름 : auth // Axios.get('api/user/auth');
                 .then(response => {
                     console.log(response) //auth(HOC)를 만들고 각각의 컴포넌트(페이지) 를 넣어줘야 함 -> App.js에서!
+
+                    if (!response.payload.isAuth) { //로그인하지 않은 상태
+                        if (option) {
+                            props.history.push('/loginPage')
+                        }
+                    } else { //로그인 한 상태
+                        if (adminRoute && !response.payload.isAdmin) {
+                            props.history.push('/');
+                        } else {
+                            if (option === false) {
+                                props.history.push('/')
+                            }
+                        }
+
+                    }
                 })
         }, [])
 
